58756354f0c600e15080bb33ff2ea3a3bd9aad1c
Jose Junior <juniorferreira1919@hotmail.com>
2024-01-04 16:52:42 -0300

    feat: add search OneToMany

--- obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796a47bbb60fe8d6c133d76c5faf8fe39c4bde03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4f142af56f221f25a7e74ec1fc77c9e142169a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
